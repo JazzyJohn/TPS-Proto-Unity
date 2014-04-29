e19db26180752cb126f2db390c147f17725dc704
JazzyJohn <vaniabocar@yahoo.com>
2014-04-29 23:54:38 +0400

    base chac class

---
+++ Gameplayobject/CharacteristicManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+//TODO GENERIC TYPE  CHARCTERISTIC CLASS + Interface Implament ob base Class
+public class CharacteristicManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
