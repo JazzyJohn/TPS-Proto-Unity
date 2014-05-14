b3bcfe711d7dbd6cd91be43cdd5dbe1e35e203cf
Dima <dimtep95@gmail.com>
2014-05-15 00:47:30 +0400

    Музыка по стадиям (без переключения стадий)

--- Sound/MusicHolder.cs
+++ Sound/MusicHolder.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 
 public class MusicHolder : MonoBehaviour {
-
+	public int[] MusicInStage;
+	
 	public AudioClip[] allClip;
-
+	
 	public float musicTimeOut;
-
+	
 	public AudioSource musicPlayer;
-
+	
+	int curStage;
+	
+	int curMusic;
+	
+	int[] MusicBefore;
+	
 	// Use this for initialization
 	void Start () {
 		musicPlayer = GetComponent<AudioSource> ();
+		MusicBefore = new int[MusicInStage.Length];
+		for (int i = 0; i < MusicBefore.Length; i++) {
+			MusicBefore[i] = MusicBeforeInt(i);
+		}
 	}
 	
 	// Update is called once per frame
@@ -20,8 +31,20 @@ public class MusicHolder : MonoBehaviour {
 			StartSong();
 		}
 	}
+	
 	public void StartSong(){
-		musicPlayer.clip = allClip [(int)(UnityEngine.Random.value * allClip.Length)];
+		if(curMusic >= MusicBefore[curStage + 1] - 1) curMusic = MusicBefore[curStage] - 1;
+		musicPlayer.clip = allClip [curMusic];
 		musicPlayer.PlayDelayed (musicTimeOut);
+		curMusic++;
+	}
+	
+	public void NextStage(){
+		if(curStage < MusicInStage.Length) curStage++;
+	}
+	
+	int MusicBeforeInt(int Stage){
+		if(Stage == 0)return MusicInStage[0];
+		return MusicInStage[Stage] + MusicBeforeInt(Stage - 1);
 	}
 }
