4f01490d11c152f11085a7c27f9e0161056f9fd2
Анатолий Малков <malan-x@yanex.ru>
2014-05-26 20:15:03 +0600

    AI начало

---
+++ Malkov/AI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AI {
+
+	public enum ConditionType {ExitTime=0, Health, Armor, Ammo, Damage, Weapone, Order}
+	public enum SubConditionType {Greater=0, Equal, Less}
+
+
+}
---
+++ Malkov/AIGraphCondition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIGraphCondition : MonoBehaviour {
+
+	//public AI.ConditionType C0;
+
+	public UIPopupList FunctionPopup;
+	public UIPopupList ArgumentPopup;
+	public UIPopupList ConditionPopup;
+	public UIPopupList ValuePopup;
+	public UIInput ValueInput;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
