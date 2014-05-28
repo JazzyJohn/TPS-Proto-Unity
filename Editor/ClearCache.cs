41b2692af9dac8b258cc63b090399271f56a312b
HAWK0044 <hawk0044>
2014-05-28 11:26:40 +0400

    Map streaming from server, you should create the scene and add empty GameObject, next you should add MapDownloader.cs script to it, then set map url and map name in the properties of MapDownloader. WARNING: MapNAME should contain the name of bundled prefab.

---
+++ Editor/ClearCache.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+/// <summary>
+/// An editor script which deletes cache info.
+///
+/// NOTE:
+///     Delete all AssetBundle content that has been cached by the current application.
+///     This function is not available to WebPlayer applications that use the shared cache.
+///
+/// See Also:
+///     http://docs.unity3d.com/Documentation/ScriptReference/Caching.CleanCache.html
+///
+/// </summary>
+public class ClearCache : ScriptableObject 
+{
+	
+	[MenuItem ("Tools/Cache/Clean")]
+	public static void CleanCache ()
+	{
+		if (Caching.CleanCache ()) 
+		{
+			Debug.LogWarning ("Successfully cleaned all caches.");
+		}
+		else 
+		{
+			Debug.LogWarning ("Cache was in use.");
+		}
+	}
+}
---
+++ Multiplayer/MapDownloader.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class MapDownloader : MonoBehaviour {
+	public static MapDownloader instance;
+	private bool instantiated;
+	
+	public string MapURL = "http://vk.rakgames.ru/kaspi/unityTest/map_kaspi_c.unity3d";
+	public string MapNAME = "MapObject";
+	public int version;
+
+	public bool isReady()
+	{
+		return instantiated;
+	}
+	
+	void Start() 
+	{	
+		instance = this;
+		instantiated = false;
+		StartCoroutine (DownloadAndCache());
+	}
+
+	public void DownLoad()
+	{
+		StartCoroutine (DownloadAndCache());
+	}
+	
+	IEnumerator DownloadAndCache (){
+		// Wait for the Caching system to be ready
+		while (!Caching.ready)
+			yield return null;
+		
+		// Load the AssetBundle file from Cache if it exists with the same version or download and store it in the cache
+		using(WWW www = WWW.LoadFromCacheOrDownload (MapURL, version))
+		{
+			yield return www;
+			
+			if (www.error != null)
+				throw new Exception("WWW map download had an error:" + www.error);
+			
+			AssetBundle bundle = www.assetBundle;
+			UnityEngine.Object[] prefabObjects = bundle.LoadAll();
+			
+			//GameObject sp = GameObject.Find("SpawnPoint");
+			//if(!sp) Debug.Log ("No sp");
+			
+			Debug.Log (prefabObjects.GetLength(0));
+			
+			for(int i=0;i<prefabObjects.GetLength(0);i++)
+			{
+				if(!prefabObjects[i]){
+					Debug.Log("No prefabObj");continue;}
+				
+				/*if(prefabObjects[i].name.ToString().StartsWith("PickUp_"))
+				{
+					Debug.Log (prefabObjects[i].name.ToString());
+					GameObject obj = null;// = Instantiate(prefabObjects[i]) as GameObject;
+					
+					if(!obj)
+						continue;
+					
+					string path = "Assets/Photon Unity Networking/Resources/";
+					path = path + obj.name;
+					path = path + ".prefab";
+					
+					PrefabUtility.CreatePrefab(path,obj,ReplacePrefabOptions.ReplaceNameBased);
+					DestroyImmediate(obj);
+					
+					//GameObject photonObj = PhotonNetwork.Instantiate(obj.name,sp.transform.position,sp.transform.rotation,0,null) as GameObject;
+				}*/
+
+				if(prefabObjects[i].name.Equals(MapNAME))
+				{
+					Debug.Log (prefabObjects[i].name.ToString());
+					GameObject obj = Instantiate(prefabObjects[i]) as GameObject;
+
+					if(!obj)
+						continue;
+
+					string path = "Assets/Photon Unity Networking/Resources/";
+					path = path + obj.name;
+					path = path + ".prefab";
+
+					PrefabUtility.CreatePrefab(path,obj,ReplacePrefabOptions.ReplaceNameBased);
+					DestroyImmediate(obj);
+				}
+			}
+			
+			//			Debug.Log (sp.transform.position);
+			
+			Debug.Log("MapDownloader has been instantiated.");
+			instantiated = true;
+			bundle.Unload(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
