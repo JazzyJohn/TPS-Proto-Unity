5fa52824de86fc0da6d62ce265a38d2404ac9270
Dima <dimtep95@gmail.com>
2014-06-12 12:46:20 +0400

    Fix

--- ControllerAnimation/AnimationManager.cs
+++ ControllerAnimation/AnimationManager.cs
@@ -282,16 +282,15 @@ public class AnimationManager : MonoBehaviour
 		public void LegSet(){
 			int Lenght = LegBones.Length-1;
 			float[] BoneLength = new float[Lenght];
-			float BonesLenght = 0;
 			for (int j = 0; j < Lenght; j++) {
 				BoneLength[j] = Vector3.Distance(LegBones[j].Bone.position, LegBones[j+1].Bone.position);
 				BonesLenght+=BoneLength[j];
+
 			}
 			for (int j = 0; j < Lenght; j++)
 				LegBones[j].Factor = 90f/BonesLenght;
 			//			LegBones[0].Factor = (BoneLength[0]/BonesLenght)*90f/BonesLenght;
-			BonesLenght = BonesLenght;
-			FootHeight = LegBones[Lenght].Bone.position.y - ZeroPoint.position.y;
+			this.FootHeight = LegBones[Lenght].Bone.position.y - ZeroPoint.position.y;
 		}
 	}
 	[Serializable]
