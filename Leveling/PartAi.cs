ede80551958949585974c2b00a23dcc2044ba2e5
Dima <dimtep95@gmail.com>
2014-07-09 20:43:39 +0400

    Part System

---
+++ Leveling/PartAi.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartAi : Part {
+	void Started(){
+		AISwarm[] ChildComp = PartTransform.GetComponentsInChildren<AISwarm>();
+		foreach (AISwarm Ai in ChildComp)
+			Ai.Init(Numb);
+	}
+}
