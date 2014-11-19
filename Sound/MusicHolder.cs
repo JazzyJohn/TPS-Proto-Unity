db02389944bb281754304ba355d75dc030101070
Ivan.Ochincenko <Ivan.Ochincenko@zed.com>
2014-11-19 16:57:16 +0300

    bug fix

--- Sound/MusicHolder.cs
+++ Sound/MusicHolder.cs
@@ -6,22 +6,27 @@ public enum MUSIC_STAGE{
 	BATLLE,
 	EXPLORATION,
 }
-
-public class MusicHolder : MonoBehaviour {
-	public int[] MusicInStage;
+[System.Serializable]
+public class MusizStage{
+	public enum MUSIC_STAGE;
+	public AudioClip[] clips;
 	
-	public AudioClip[] allClip;
+}
+public class MusicHolder : MonoBehaviour {
+	public MusizStage[] MusicInStage;
+	
+
 	
 	public float musicTimeOut;
 	
 	public AudioSource musicPlayer;
 
-    public int curStage;
+    public MusizStage curStage;
 	
-	int curMusic;
+	public int curStageInt;
 	
-	int[] MusicBefore;
-	
+	AudioClip curMusic;
+			
 	private static float volume = 1.0f;
 	
 	public static void  SetVolume(float newVolume){
@@ -39,13 +44,7 @@ public class MusicHolder : MonoBehaviour {
 	void Awake () {
 		musicPlayer = GetComponent<AudioSource> ();
         musicPlayer.ignoreListenerVolume = true;
-		MusicBefore = new int[MusicInStage.Length];
-		int tempMusic = 0;
-		for (int i = 0; i < MusicBefore.Length; i++) {
-			MusicBefore[i] = MusicInStage[i] + tempMusic;
-			tempMusic += MusicInStage[i];
-			Debug.Log(MusicBefore[i]);
-		}
+		
 
 	}
 	void Start(){
@@ -61,35 +60,26 @@ public class MusicHolder : MonoBehaviour {
         
         if ( GameRule.instance!=null&&curStage != (int)GameRule.instance.curStage)
         {
-            curStage = (int)GameRule.instance.curStage;
+            curStageInt = (int)GameRule.instance.curStage;
 			NextStage();
 		}
 	}
 	
 	public void StartSong(){
-		if (curStage == 0) {
-			curMusic = (int)UnityEngine.Random.Range (0, MusicBefore [curStage]);
-		}
-		else {
-            
-			curMusic = (int)UnityEngine.Random.Range (MusicBefore [curStage - 1], MusicBefore [curStage]);
-         
-			if(curMusic>allClip.Length) curMusic = MusicBefore[curStage-1] - 1;
-		}
+		
+		int	curMusicInt = (int)UnityEngine.Random.Range (0, curStage.clips.Length);
+		curMusic = curStage.clips[curMusicInt];
         Debug.Log("NOW PLAY" + curMusic); 
-        musicPlayer.clip = allClip[curMusic];
+        musicPlayer.clip = curMusic;
 		musicPlayer.PlayDelayed (musicTimeOut);
 
 	}
 	
 	public void NextStage(){
         Debug.Log("NEW STAGE " + MusicInStage.Length);
-		if (curStage <= MusicInStage.Length - 1) {
-				//curStage++;
-				//curMusic = MusicBefore[curStage] - 1;
-            Debug.Log("NEW STAGE " + curStage);
-				musicPlayer.Stop ();
-				
+		if (curStageInt <MusicInStage.Length ) {
+			curStage = MusicInStage[curStageInt]
+		    StartSong();
 		}
 	}
 }
